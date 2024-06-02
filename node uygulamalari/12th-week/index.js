a4a22b3eabb18259f57a277dc6317f93ad3285f7
Hudakasim <030722088@std.izu.edu.tr>
2024-06-02 14:09:30 +0300

    completed

--- node uygulamalari/12th-week/index.js
+++ node uygulamalari/12th-week/index.js
@@ -1,6 +1,27 @@
-// express --> Yazıda Node.js ile web sunucusuna gelen istekleri yönetmek, düzenlemek ve yayınlamak
+// express --> web sunucusuna gelen istekleri yönetmek, düzenlemek ve yayınlamak
 const express = require('express');
-const app = express;
-const port = 7008;
+const app = express();
+const port = 7088;
 
-app.get
+// HTTP GET istegi icin bir rota tanimladik
+// rota = ana dizin '/'
+app.get("/", function(req, res){
+	res.send("ornek");
+});
+
+// baska bir rota tanimladik
+// rota = '/iletisimm' URL
+app.get("/iletisimm", (req, res) => {
+	res.sendFile(__dirname + "/iletisim.html");
+});
+
+app.get("/FormuGonder", (req, res) => {
+	kullanici = {isim: req.query.name, soyisim: req.query.surname,}; // name ve surname html dosyasindaki
+	console.log(kullanici.isim + " " + kullanici.soyisim);
+	res.send(kullanici.isim + " " + kullanici.soyisim);
+});
+
+const server = app.listen(port, () =>{
+	console.log("dinleniyor");
+});
+// __dirname --> o anki dosyanin bulundugu dizinin mutlak yolunu donduren bir degiskendir
